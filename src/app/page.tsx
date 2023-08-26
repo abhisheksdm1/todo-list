247f4d63d29795524d854df61007a79e4a7e5414
abhishek <abhishek@maitretech.com>
2023-08-26 18:14:54 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 export default function Home() {
 
   const [listValue,setListValue]=useState<string>("")
-  const [array,setArray]=useState<string[]>([])
+  const [listArray,setListArray]=useState<string[]>([])
   function handle(e: React.FormEvent<HTMLInputElement>)
   {
       setListValue(e.currentTarget.value)
@@ -12,21 +12,28 @@ export default function Home() {
 
   function handleKeywordKeyPress()
   {
-    setArray((prevArray)=>[...prevArray , listValue])
+    setListArray((prevArray)=>[...prevArray , listValue])
     setListValue("");
   }
 
+  function deleteHandler(index:number)
+  {
+    const deletedList=[...listArray]
+    // deletedList.splice(index, 1);
+    delete deletedList[index];
+    setListArray(deletedList);
+  }
 
-
+  
   return (
     <main className="flex min-h-screen flex-col">
         <input type='text' placeholder='search' onChange={(e)=>handle(e)} value={listValue}/>
         <button onClick={handleKeywordKeyPress}>Click</button>
         {
-          array.map((item,index)=>{
+          listArray.map((item,index)=>{
             return(
               <div key={index}>
-                <h1>{item}<span><button style={{background:'yellow'}}>Edit</button></span><span><button style={{background:'red'}}>delete</button></span></h1>
+                <h1>{item}<span><button style={{background:'yellow'}} >Edit</button></span><span><button onClick={()=>deleteHandler(index)} style={{background:'red'}}>delete</button></span></h1>
               </div>
             )
           })
