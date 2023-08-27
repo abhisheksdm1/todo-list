86c0abfcb34dc95a50d4483bdf3c4213c750201d
abhishek <abhishek@maitretech.com>
2023-08-27 13:54:16 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import React, { useState } from 'react';
 
 export default function Home() {
 
-  const [listValue,setListValue]=useState<string>("")
-  const [listArray,setListArray]=useState<string[]>([])
+  const [listValue,setListValue]=useState<string>("");
+  const [listArray,setListArray]=useState<string[]>([]);
+  const [toggle,setToggle]=useState<boolean>(false);
+  const [index,setIndex]=useState<any>();
   function handle(e: React.FormEvent<HTMLInputElement>)
   {
       setListValue(e.currentTarget.value)
@@ -12,18 +14,33 @@ export default function Home() {
 
   function handleKeywordKeyPress()
   {
-    setListArray((prevArray)=>[...prevArray , listValue])
+    if(toggle===true)
+    {
+    setListArray((prevArray)=>[prevArray[index]=listValue])
+    setToggle((prevStae)=>!prevStae)
+  } 
+    else {setListArray((prevArray)=>[...prevArray , listValue])
+     
+    }
     setListValue("");
   }
 
   function deleteHandler(index:number)
   {
+    
     const deletedList=[...listArray]
     deletedList.splice(index, 1);
     // delete deletedList[index];
-    setListArray(deletedList);
+    setListArray(()=>deletedList);
   }
 
+  function editHandler(item:string ,index:number)
+  {
+    setIndex(index);
+    setListValue(()=>item)
+    setToggle((prevStae)=>!prevStae)
+
+  }
   
   return (
     <main className="flex min-h-screen flex-col">
@@ -33,7 +50,7 @@ export default function Home() {
           listArray.map((item,index)=>{
             return(
               <div key={index}>
-                <h1>{item}<span><button style={{background:'yellow'}} >Edit</button></span><span><button onClick={()=>deleteHandler(index)} style={{background:'red'}}>delete</button></span></h1>
+                <h1>{item}<span><button style={{background:'yellow'}} onClick={()=>editHandler(item,index)} >Edit</button></span><span><button onClick={()=>deleteHandler(index)} style={{background:'red'}}>delete</button></span></h1>
               </div>
             )
           })
